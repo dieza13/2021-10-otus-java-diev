84d38b2af24b36896ed081c4421f04e47ad9e7db
DievAN <DievAN@alfastrah.ru>
2021-10-30 23:26:11 +0300

    Fourth HW: gc work research

---
+++ hw04-gc/homework/src/main/java/ru/calculator/Data.java
@@ -0,0 +1,22 @@
+package ru.calculator;
+
+public class Data {
+    private final int value;
+//    private final Integer value;
+//
+//    public Data(Integer value) {
+//        this.value = value;
+//    }
+//
+//    public Integer getValue() {
+//        return value;
+//    }
+
+    public Data(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
