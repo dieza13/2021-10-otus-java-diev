8405a6de6317f06472767eda6da9a74df3fdddc2
DievAN <DievAN@alfastrah.ru>
2021-12-18 16:49:28 +0300

    Seventh HW: remove excess call

--- hw07-structuralPatterns/homework/src/main/java/ru/otus/processor/homework/ProcessorEvenSecondThrowError.java
+++ hw07-structuralPatterns/homework/src/main/java/ru/otus/processor/homework/ProcessorEvenSecondThrowError.java
@@ -3,7 +3,6 @@ package ru.otus.processor.homework;
 import ru.otus.model.Message;
 import ru.otus.processor.Processor;
 
-import java.time.LocalDateTime;
 
 public class ProcessorEvenSecondThrowError implements Processor {
 
@@ -15,7 +14,6 @@ public class ProcessorEvenSecondThrowError implements Processor {
 
     @Override
     public Message process(Message message) {
-        LocalDateTime ld = localDateTimeProvider.getDateTime();
         if (localDateTimeProvider.getDateTime().getSecond() % 2 == 0) {
             throw new RuntimeException("Я работаю только по четным секундам месяца");
         }
